2e84de5ef3aa96e77f11331e8ebf5a71b27eeb7b
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2024-04-13 11:13:49 +0530

    set up Chat Engine API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
+      userName="alan"
+      userSecret="123"
+    />
+  );
 }
 
 export default App;
